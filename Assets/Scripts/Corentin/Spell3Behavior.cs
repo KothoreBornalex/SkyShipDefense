5cecf58032e1149f9e077032c098bc104411a868
Corentinrmt <corentin.remot@gmail.com>
2023-10-24 11:30:20 +0200

    Spells 2 and 3 behavior
    Creates spell 2 and 3 script behavior and add destroy cooldown

---
+++ Assets/Scripts/Corentin/Spell3Behavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell3Behavior : AttackBehavior
+{
+    // Fields
+
+    [SerializeField] private float _duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpellTimeCast()
+    {
+        yield return new WaitForSeconds(_duration);
+
+        Destroy(gameObject);
+        yield return null;
+    }
+}
